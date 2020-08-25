837f0884690cdb59d10cf8b28744749c931081fc
https://learn.co/arf2k <foreman.alexander@gmail.com>
2020-08-25 17:06:17 -0400

    idk why it's not passing this is wack

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts, only: [:index, :show, :new, :create], :as => "post"
+
+
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
